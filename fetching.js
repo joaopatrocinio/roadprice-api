b90501c7aedcacaf83bfa9a3614444aa5f55a760
João Patrocínio <jpatrocinio5436@hotmail.com>
2020-12-19 14:49:11 +0000

    Combustivel barato

--- fetching.js
+++ fetching.js
@@ -41,10 +41,41 @@ module.exports = () => {
                 preco: dados[0].children[14].children[1].data
             }
         ];
+        const baratos = $("#homeCheap");
+        const precosBaratos = [
+            {
+                tipo: "gasolina95simples",
+                preco: baratos[0].children[2].children[1].data
+            },
+            {
+                tipo: "gasolina95plus",
+                preco: baratos[0].children[4].children[1].data
+            },
+            {
+                tipo: "gasolina98simples",
+                preco: baratos[0].children[6].children[1].data
+            },
+            {
+                tipo: "gasolina98plus",
+                preco: baratos[0].children[8].children[1].data
+            },
+            {
+                tipo: "gasoleoSimples",
+                preco: baratos[0].children[10].children[1].data
+            },
+            {
+                tipo: "gasoleoPlus",
+                preco: baratos[0].children[12].children[1].data
+            },
+            {
+                tipo: "gplAuto",
+                preco: baratos[0].children[14].children[1].data
+            }
+        ];
         let i = 0;
         function inserirPreco() {
             if (i < precos.length) {
-                db.query("INSERT INTO combustivel (combustivel_tipo, combustivel_preco, combustivel_data) VALUES (?, ?, ?)", [precos[i].tipo, precos[i].preco.substring(1, precos[i].preco.length), moment().format("YYYY-MM-DD")], function (err, result) {
+                db.query("INSERT INTO combustivel (combustivel_tipo, combustivel_preco_medio, combustivel_preco_barato, combustivel_data) VALUES (?, ?, ?)", [precos[i].tipo, precos[i].preco.substring(1, precos[i].preco.length), precosBaratos[i].preco.substring(1, precosBaratos[i].preco.length), moment().format("YYYY-MM-DD")], function (err, result) {
                     if (err) throw err;
                     console.log(i);
                     i++;
--- routes.js
+++ routes.js
@@ -15,11 +15,12 @@ const combustiveis = [
 
 router.get("/preco/:combustivel", (req, res) => {
     if (combustiveis.find(c => c == req.params.combustivel)) {
-        db.query("SELECT combustivel_tipo, combustivel_preco, combustivel_data FROM combustivel WHERE combustivel_tipo = ? ORDER BY combustivel_data DESC LIMIT 1", [req.params.combustivel], (err, result) => {
+        db.query("SELECT combustivel_tipo, combustivel_preco_medio, combustivel_preco_barato, combustivel_data FROM combustivel WHERE combustivel_tipo = ? ORDER BY combustivel_data DESC LIMIT 1", [req.params.combustivel], (err, result) => {
             if (err) throw err;
             return res.json({
                 tipo: result[0].combustivel_tipo,
-                preco: result[0].combustivel_preco,
+                preco_medio: result[0].combustivel_preco_medio,
+                preco_barato: result[0].combustivel_preco_barato,
                 data: moment(result[0].combustivel_data).format("YYYY-MM-DD"),
             })
         })
