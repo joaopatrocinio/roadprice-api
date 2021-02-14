2acdcd6fa1f15be2182d03d3950a77c2a50cf74e
João Patrocínio <jpatrocinio5436@hotmail.com>
2021-02-14 17:02:08 +0000

    Editar password

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const dotenv = require("dotenv").config();
 const cron = require('node-cron');
+const bcrypt = require("bcryptjs");
 const cors = require("cors");
 
 const app = express()
--- routes/user.js
+++ routes/user.js
@@ -1,14 +1,31 @@
 const express = require("express");
+const bcrypt = require("bcryptjs");
 const router = express.Router();
 
 const db = require("../db.js");
 
 router.post("/editarperfil", (req, res) => {
     if (req.body.user_fullname) {
-        db.query("UPDATE user SET user_fullname = ? WHERE user_id = ?", [req.body.user_fullname, req.user.user_id], (err, result) => {
-            if (err) return res.status(500).json({ message: 'Ocorreu um erro a identificar o utilizador.' });
-            res.json({ message: "Perfil editado com sucesso." })
-        })
+        if (req.body.user_password && req.body.user_old_password) {
+            var salt = bcrypt.genSaltSync(10);
+            var hash = bcrypt.hashSync(req.body.user_password, salt);
+            db.query("SELECT user_password FROM user WHERE user_id = ?", [req.user.user_id], (err, result) => {
+                if (err) return res.status(500).json({ message: 'Ocorreu um erro a identificar o utilizador.' });
+                var passwordIsValid = bcrypt.compareSync(req.body.user_old_password, result[0].user_password);
+                if (!passwordIsValid) return res.status(403).json({ message: "Password incorreta." })
+                db.query("UPDATE user SET user_fullname = ?, user_password = ? WHERE user_id = ?", [req.body.user_fullname, hash, req.user.user_id], (err, result) => {
+                    if (err) return res.status(500).json({ message: 'Ocorreu um erro a identificar o utilizador.' });
+                    res.json({ message: "Perfil editado com sucesso." })
+                })
+            })
+        } else {
+            db.query("UPDATE user SET user_fullname = ? WHERE user_id = ?", [req.body.user_fullname, req.user.user_id], (err, result) => {
+                if (err) return res.status(500).json({ message: 'Ocorreu um erro a identificar o utilizador.' });
+                res.json({ message: "Perfil editado com sucesso." })
+            })
+        }
+    } else {
+        res.status(400).json({ message: "Campos em falta." })
     }
 })
 
